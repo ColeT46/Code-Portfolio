b4f7db9350beab58b04bac81d7803d222fb5b11a
unknown <colethomson46@gmail.com>
2019-10-22 12:49:29 -0500

    Added alternative method to get common elements in array with better space complexity

--- Misc Java Programs/CommonElements.java
+++ Misc Java Programs/CommonElements.java
@@ -8,12 +8,13 @@ public class CommonElements {
 	/**
 	 * Gets the common elements of two arrays and returns an Integer array
 	 * of elements in common. (Assumes input arrays are sorted)
-	 * Time Complexity: O(n)
+	 * Time Complexity: O(max(m,n))
+	 * Space Complexity: O(m)
 	 * @param arr1 
 	 * @param arr2
 	 * @return retArr - array containing common elements
 	 */
-	public static int[] getCommonElements(int[] arr1, int[] arr2) {
+	public static int[] getCommonElements1(int[] arr1, int[] arr2) {
 		HashSet<Integer> set1 = new HashSet<Integer>();
 		ArrayList<Integer> combined = new ArrayList<Integer>();
 		int[] retArr;
@@ -39,12 +40,58 @@ public class CommonElements {
 	}
 	
 	/**
+	 * Alternative approach to getting the elements in common of two arrays.
+	 * This method benefits by saving space due to the removal of the Set used
+	 * to store one of the arrays. (Assumes input arrays are sorted)
+	 * Time Complexity: O(max(m,n))
+	 * Space Complexity: O(c) where c is # elements arrays have in common 
+	 * @param arr1
+	 * @param arr2
+	 * @return retArr - array containing common elements
+	 */
+	public static int[] getCommonElements2(int[] arr1, int[] arr2) {
+		ArrayList<Integer> combined = new ArrayList<Integer>();
+		int[] retArr;
+		int pointer1 = 0;	// Pointer at index in arr1
+		int pointer2 = 0;	// Pointer at index in arr2
+		
+		// While pointers are valid indices, compare array elements
+		while (pointer1 < arr1.length && pointer2 < arr2.length) {
+			// check if elements are equal, add to combined and increment pointers
+			if (arr1[pointer1] == arr2[pointer2]) {
+				combined.add(arr1[pointer1]);
+				++pointer1;
+				++pointer2;
+			}
+			// else if element at pointer 1 is greater than element at pointer 2
+			else if (arr1[pointer1] > arr2[pointer2]) {
+				// increment pointer 2
+				++pointer2;
+			}
+			// else element at pointer 1 is less than element at pointer 2
+			else {
+				++pointer1;
+			}
+		}
+
+		retArr = new int[combined.size()];
+		// transform ArrayList to int[] array
+		for (int i = 0; i < combined.size(); i++) {
+			retArr[i] = combined.get(i);
+		}
+		
+		return retArr;
+	}
+	
+	/**
 	 * Main method used to test getCommonElements method.
 	 * @param args - unused
 	 */
 	public static void main(String[] args) {
 		int numTests = 4;
-		int numPassed = 0;
+		int numPassed1 = 0;
+		int numPassed2 = 0;
+		
 		// Test Arrays (1)
 		int[][] testArr1 = {{1,3,4,6,7,9},
 							{1,2,5,7},
@@ -60,28 +107,48 @@ public class CommonElements {
 							 {1,2},
 						 	 {},
 						 	 {}};
-		// Actual outputs
-		int[][] actOut = new int[4][];
+		// Actual outputs for method: getCommonElements1
+		int[][] actOut1 = new int[numTests][];
+		// Actual outputs for method: getCommonElements2
+		int[][] actOut2 = new int[numTests][];
 		
 		// for each test
 		for (int i = 0; i < testArr1.length; i++) {
-			actOut[i] = getCommonElements(testArr1[i], testArr2[i]);
-			// check if matches expected
-			if (Arrays.equals(actOut[i], expOut[i])) {
-				numPassed++;
+			actOut1[i] = getCommonElements1(testArr1[i], testArr2[i]);
+			actOut2[i] = getCommonElements2(testArr1[i], testArr2[i]);
+			
+			// check if matches expected for method 1
+			if (Arrays.equals(actOut1[i], expOut[i])) {
+				numPassed1++;
 			} else {
-				System.out.println("getCommonElementsTest0" + i + "FAILED");
+				System.out.println("getCommonElements1Test0" + i + "FAILED");
 				System.out.println("-Input Arr 1: " + testArr1[i]);
 				System.out.println("-Input Arr 2: " + testArr2[i]);
-				System.out.println("-Expected Result: " + actOut[i]);
-				System.out.println("-Actual Result: " + expOut[i]);
+				System.out.println("-Expected Result: " + expOut[i]);
+				System.out.println("-Actual Result: " + actOut1[i]);
 				
+			}
+			
+			// check if matches expected for method 2
+			if (Arrays.equals(actOut2[i], expOut[i])) {
+				numPassed2++;
+			} else {
+				System.out.println("getCommonElements2Test0" + i + "FAILED");
+				System.out.println("-Input Arr 1: " + testArr1[i]);
+				System.out.println("-Input Arr 2: " + testArr2[i]);
+				System.out.println("-Expected Result: " + expOut[i]);
+				System.out.println("-Actual Result: " + actOut2[i]);
+
 			}
 		}
 		
-		// Print Results
-		System.out.println("getCommonElements Test Results:");
-		System.out.println("- # Passed: " + numPassed);
+		// Print Results - method 1
+		System.out.println("getCommonElements1 Test Results:");
+		System.out.println("- # Passed: " + numPassed1);
+		System.out.println("- # Tests: " + numTests);
+		// Print Results - method 2
+		System.out.println("getCommonElements2 Test Results:");
+		System.out.println("- # Passed: " + numPassed2);
 		System.out.println("- # Tests: " + numTests);
 
 	}
